ab422b179c804e669cadfd7a57e1c64d029c8c83
aztecbinaynay <qjeabinay@tip.edu.ph>
2023-06-14 08:02:04 +0800

    finished the code to retrieve data from database

--- Flask_backend.py
+++ Flask_backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import sqlite3
 
 app = Flask(__name__)
@@ -59,6 +59,57 @@ def insert_data():
         conn.close()
         print("Error", e)
         return "Data not inserted"
+
+
+@app.route("/retrieve", methods=["POST"])
+def get_data():
+    try:
+        data = request.get_json()  # Get the JSON data from the request
+        conn = sqlite3.connect(
+            r"C:\Users\Toshiba\Documents\PD2_john\databases\SensorReadings.db"
+        )
+        cursor = conn.cursor()
+
+        # Begin a transaction
+        conn.execute("BEGIN TRANSACTION")
+
+        cursor.execute("SELECT * FROM SensorReadings WHERE UserID=?", (data["UserID"],))
+
+        # Fetch the row from the result
+        row = cursor.fetchone()
+
+        if row is not None:
+            # Extract the values from the row
+            id_, UserID, Therm, ECG, Airflow, Snore, SpO2, HR, TimeIn, TimeOut = row
+
+            # Create a dictionary to store the retrieved data
+            data_dict2 = {
+                "UserID": UserID,
+                "Therm": Therm,
+                "ECG": ECG,
+                "Airflow": Airflow,
+                "Snore": Snore,
+                "SpO2": SpO2,
+                "HR": HR,
+                "TimeIn": TimeIn,
+                "TimeOut": TimeOut,
+            }
+            conn.commit()
+            conn.close()
+            print("Data retrieved")
+            return jsonify(data_dict2), 200
+
+        conn.commit()
+        conn.close()
+        print("Data unavailable")
+        return "row does not exist", 404
+
+    except Exception as e:
+        # Rollback the transaction if an error occurs
+        conn.rollback()
+        conn.close()
+        print("Error", e)
+        return str(e), 500
 
 
 if __name__ == "__main__":
